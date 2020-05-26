fe214c44eabb5592a876e89a2da6713c56061487
B.Topchiyski <topchiisky@gmail.com>
2020-05-26 13:23:51 +0300

    BATEEEEE

--- HeroWarsGame4.28/HeroWarsGame/Form1.designer.cs
+++ HeroWarsGame4.28/HeroWarsGame/Form1.designer.cs
@@ -32,6 +32,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Battle));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
+            this.Mob1 = new System.Windows.Forms.PictureBox();
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.BigShield = new System.Windows.Forms.PictureBox();
+            this.MobShot = new System.Windows.Forms.Panel();
+            this.PlayerShot = new System.Windows.Forms.Panel();
+            this.Fire2 = new System.Windows.Forms.PictureBox();
+            this.Fire1 = new System.Windows.Forms.PictureBox();
+            this.Bomb1 = new System.Windows.Forms.PictureBox();
+            this.Bomb2 = new System.Windows.Forms.PictureBox();
             this.PlayerHealth = new System.Windows.Forms.Label();
             this.MobHealth = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -44,18 +53,8 @@
             this.GoldAmount = new System.Windows.Forms.Label();
             this.Shield = new System.Windows.Forms.PictureBox();
             this.Heal = new System.Windows.Forms.PictureBox();
-            this.Mob1 = new System.Windows.Forms.PictureBox();
-            this.Player2 = new System.Windows.Forms.PictureBox();
-            this.BigShield = new System.Windows.Forms.PictureBox();
-            this.MobShot = new System.Windows.Forms.Panel();
-            this.PlayerShot = new System.Windows.Forms.Panel();
-            this.Fire2 = new System.Windows.Forms.PictureBox();
-            this.Fire1 = new System.Windows.Forms.PictureBox();
-            this.Bomb1 = new System.Windows.Forms.PictureBox();
-            this.Bomb2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Shield)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Heal)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Mob1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BigShield)).BeginInit();
@@ -63,6 +62,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.Fire1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Bomb1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Bomb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Shield)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Heal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
@@ -73,22 +75,136 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Controls.Add(this.Mob1);
-            this.panel1.Controls.Add(this.Player2);
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel1.Controls.Add(this.BigShield);
+            this.panel1.Controls.Add(this.Player2);
+            this.panel1.Controls.Add(this.Mob1);
             this.panel1.Controls.Add(this.MobShot);
             this.panel1.Controls.Add(this.PlayerShot);
             this.panel1.Controls.Add(this.Fire2);
             this.panel1.Controls.Add(this.Fire1);
             this.panel1.Controls.Add(this.Bomb1);
             this.panel1.Controls.Add(this.Bomb2);
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(0, 40);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(678, 492);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Mob1
+            // 
+            this.Mob1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Mob1.BackColor = System.Drawing.Color.Transparent;
+            this.Mob1.Image = global::HeroWarsGame.Properties.Resources.GoblinMage;
+            this.Mob1.Location = new System.Drawing.Point(633, 170);
+            this.Mob1.Margin = new System.Windows.Forms.Padding(2);
+            this.Mob1.Name = "Mob1";
+            this.Mob1.Size = new System.Drawing.Size(43, 62);
+            this.Mob1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Mob1.TabIndex = 0;
+            this.Mob1.TabStop = false;
+            // 
+            // Player2
+            // 
+            this.Player2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Player2.BackColor = System.Drawing.Color.Transparent;
+            this.Player2.Image = global::HeroWarsGame.Properties.Resources.HumanMArcher_B;
+            this.Player2.Location = new System.Drawing.Point(2, 179);
+            this.Player2.Margin = new System.Windows.Forms.Padding(2);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(47, 64);
+            this.Player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Player2.TabIndex = 0;
+            this.Player2.TabStop = false;
+            // 
+            // BigShield
+            // 
+            this.BigShield.BackColor = System.Drawing.Color.Transparent;
+            this.BigShield.Image = ((System.Drawing.Image)(resources.GetObject("BigShield.Image")));
+            this.BigShield.Location = new System.Drawing.Point(50, 257);
+            this.BigShield.Margin = new System.Windows.Forms.Padding(2);
+            this.BigShield.Name = "BigShield";
+            this.BigShield.Size = new System.Drawing.Size(45, 54);
+            this.BigShield.TabIndex = 12;
+            this.BigShield.TabStop = false;
+            this.BigShield.Visible = false;
+            // 
+            // MobShot
+            // 
+            this.MobShot.BackColor = System.Drawing.Color.Transparent;
+            this.MobShot.BackgroundImage = global::HeroWarsGame.Properties.Resources.arrow21;
+            this.MobShot.Location = new System.Drawing.Point(603, 193);
+            this.MobShot.Margin = new System.Windows.Forms.Padding(2);
+            this.MobShot.Name = "MobShot";
+            this.MobShot.Size = new System.Drawing.Size(26, 11);
+            this.MobShot.TabIndex = 1;
+            // 
+            // PlayerShot
+            // 
+            this.PlayerShot.BackColor = System.Drawing.Color.Transparent;
+            this.PlayerShot.BackgroundImage = global::HeroWarsGame.Properties.Resources.lightning;
+            this.PlayerShot.Location = new System.Drawing.Point(53, 193);
+            this.PlayerShot.Margin = new System.Windows.Forms.Padding(2);
+            this.PlayerShot.Name = "PlayerShot";
+            this.PlayerShot.Size = new System.Drawing.Size(26, 11);
+            this.PlayerShot.TabIndex = 0;
+            this.PlayerShot.Visible = false;
+            // 
+            // Fire2
+            // 
+            this.Fire2.BackColor = System.Drawing.Color.Transparent;
+            this.Fire2.Image = ((System.Drawing.Image)(resources.GetObject("Fire2.Image")));
+            this.Fire2.Location = new System.Drawing.Point(8, 257);
+            this.Fire2.Margin = new System.Windows.Forms.Padding(2);
+            this.Fire2.Name = "Fire2";
+            this.Fire2.Size = new System.Drawing.Size(34, 37);
+            this.Fire2.TabIndex = 10;
+            this.Fire2.TabStop = false;
+            this.Fire2.Visible = false;
+            this.Fire2.Click += new System.EventHandler(this.Fire2_Click);
+            // 
+            // Fire1
+            // 
+            this.Fire1.BackColor = System.Drawing.Color.Transparent;
+            this.Fire1.Image = ((System.Drawing.Image)(resources.GetObject("Fire1.Image")));
+            this.Fire1.Location = new System.Drawing.Point(8, 257);
+            this.Fire1.Margin = new System.Windows.Forms.Padding(2);
+            this.Fire1.Name = "Fire1";
+            this.Fire1.Size = new System.Drawing.Size(34, 37);
+            this.Fire1.TabIndex = 9;
+            this.Fire1.TabStop = false;
+            this.Fire1.Visible = false;
+            // 
+            // Bomb1
+            // 
+            this.Bomb1.BackColor = System.Drawing.Color.Transparent;
+            this.Bomb1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Bomb1.BackgroundImage")));
+            this.Bomb1.Location = new System.Drawing.Point(8, 298);
+            this.Bomb1.Margin = new System.Windows.Forms.Padding(2);
+            this.Bomb1.Name = "Bomb1";
+            this.Bomb1.Size = new System.Drawing.Size(38, 41);
+            this.Bomb1.TabIndex = 9;
+            this.Bomb1.TabStop = false;
+            this.Bomb1.Visible = false;
+            // 
+            // Bomb2
+            // 
+            this.Bomb2.BackColor = System.Drawing.Color.Transparent;
+            this.Bomb2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Bomb2.BackgroundImage")));
+            this.Bomb2.Location = new System.Drawing.Point(8, 298);
+            this.Bomb2.Margin = new System.Windows.Forms.Padding(2);
+            this.Bomb2.Name = "Bomb2";
+            this.Bomb2.Size = new System.Drawing.Size(38, 41);
+            this.Bomb2.TabIndex = 11;
+            this.Bomb2.TabStop = false;
+            this.Bomb2.Visible = false;
             // 
             // PlayerHealth
             // 
@@ -211,115 +327,15 @@
             this.Heal.Click += new System.EventHandler(this.Heal_Click);
             this.Heal.MouseHover += new System.EventHandler(this.Heal_MouseHover);
             // 
-            // Mob1
+            // pictureBox1
             // 
-            this.Mob1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Mob1.BackColor = System.Drawing.Color.Transparent;
-            this.Mob1.Image = global::HeroWarsGame.Properties.Resources.GoblinMage;
-            this.Mob1.Location = new System.Drawing.Point(643, 170);
-            this.Mob1.Margin = new System.Windows.Forms.Padding(2);
-            this.Mob1.Name = "Mob1";
-            this.Mob1.Size = new System.Drawing.Size(29, 50);
-            this.Mob1.TabIndex = 0;
-            this.Mob1.TabStop = false;
-            // 
-            // Player2
-            // 
-            this.Player2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Player2.BackColor = System.Drawing.Color.Transparent;
-            this.Player2.Image = global::HeroWarsGame.Properties.Resources.ElfFGunner_B;
-            this.Player2.Location = new System.Drawing.Point(2, 170);
-            this.Player2.Margin = new System.Windows.Forms.Padding(2);
-            this.Player2.Name = "Player2";
-            this.Player2.Size = new System.Drawing.Size(35, 50);
-            this.Player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.Player2.TabIndex = 0;
-            this.Player2.TabStop = false;
-            // 
-            // BigShield
-            // 
-            this.BigShield.Image = ((System.Drawing.Image)(resources.GetObject("BigShield.Image")));
-            this.BigShield.Location = new System.Drawing.Point(46, 257);
-            this.BigShield.Margin = new System.Windows.Forms.Padding(2);
-            this.BigShield.Name = "BigShield";
-            this.BigShield.Size = new System.Drawing.Size(45, 63);
-            this.BigShield.TabIndex = 12;
-            this.BigShield.TabStop = false;
-            this.BigShield.Visible = false;
-            // 
-            // MobShot
-            // 
-            this.MobShot.BackColor = System.Drawing.Color.Transparent;
-            this.MobShot.BackgroundImage = global::HeroWarsGame.Properties.Resources.arrow21;
-            this.MobShot.Location = new System.Drawing.Point(614, 193);
-            this.MobShot.Margin = new System.Windows.Forms.Padding(2);
-            this.MobShot.Name = "MobShot";
-            this.MobShot.Size = new System.Drawing.Size(26, 11);
-            this.MobShot.TabIndex = 1;
-            // 
-            // PlayerShot
-            // 
-            this.PlayerShot.BackColor = System.Drawing.Color.Transparent;
-            this.PlayerShot.BackgroundImage = global::HeroWarsGame.Properties.Resources.lightning;
-            this.PlayerShot.Location = new System.Drawing.Point(41, 193);
-            this.PlayerShot.Margin = new System.Windows.Forms.Padding(2);
-            this.PlayerShot.Name = "PlayerShot";
-            this.PlayerShot.Size = new System.Drawing.Size(26, 11);
-            this.PlayerShot.TabIndex = 0;
-            this.PlayerShot.Visible = false;
-            // 
-            // Fire2
-            // 
-            this.Fire2.BackColor = System.Drawing.Color.Transparent;
-            this.Fire2.Image = ((System.Drawing.Image)(resources.GetObject("Fire2.Image")));
-            this.Fire2.Location = new System.Drawing.Point(8, 257);
-            this.Fire2.Margin = new System.Windows.Forms.Padding(2);
-            this.Fire2.Name = "Fire2";
-            this.Fire2.Size = new System.Drawing.Size(34, 37);
-            this.Fire2.TabIndex = 10;
-            this.Fire2.TabStop = false;
-            this.Fire2.Visible = false;
-            this.Fire2.Click += new System.EventHandler(this.Fire2_Click);
-            // 
-            // Fire1
-            // 
-            this.Fire1.BackColor = System.Drawing.Color.Transparent;
-            this.Fire1.Image = ((System.Drawing.Image)(resources.GetObject("Fire1.Image")));
-            this.Fire1.Location = new System.Drawing.Point(8, 257);
-            this.Fire1.Margin = new System.Windows.Forms.Padding(2);
-            this.Fire1.Name = "Fire1";
-            this.Fire1.Size = new System.Drawing.Size(34, 37);
-            this.Fire1.TabIndex = 9;
-            this.Fire1.TabStop = false;
-            this.Fire1.Visible = false;
-            // 
-            // Bomb1
-            // 
-            this.Bomb1.BackColor = System.Drawing.Color.Transparent;
-            this.Bomb1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Bomb1.BackgroundImage")));
-            this.Bomb1.Location = new System.Drawing.Point(8, 298);
-            this.Bomb1.Margin = new System.Windows.Forms.Padding(2);
-            this.Bomb1.Name = "Bomb1";
-            this.Bomb1.Size = new System.Drawing.Size(38, 41);
-            this.Bomb1.TabIndex = 9;
-            this.Bomb1.TabStop = false;
-            this.Bomb1.Visible = false;
-            // 
-            // Bomb2
-            // 
-            this.Bomb2.BackColor = System.Drawing.Color.Transparent;
-            this.Bomb2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Bomb2.BackgroundImage")));
-            this.Bomb2.Location = new System.Drawing.Point(8, 298);
-            this.Bomb2.Margin = new System.Windows.Forms.Padding(2);
-            this.Bomb2.Name = "Bomb2";
-            this.Bomb2.Size = new System.Drawing.Size(38, 41);
-            this.Bomb2.TabIndex = 11;
-            this.Bomb2.TabStop = false;
-            this.Bomb2.Visible = false;
+            this.pictureBox1.Image = global::HeroWarsGame.Properties.Resources.Battlefield;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(677, 492);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
             // 
             // Battle
             // 
@@ -340,7 +356,7 @@
             this.Controls.Add(this.PlayerHealth);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Battle";
@@ -348,8 +364,6 @@
             this.Load += new System.EventHandler(this.Battle_Load);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.w);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.Shield)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Heal)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Mob1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.BigShield)).EndInit();
@@ -357,6 +371,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.Fire1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Bomb1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Bomb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Shield)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Heal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -387,6 +404,7 @@
         private System.Windows.Forms.Label GoldAmount;
         private System.Windows.Forms.PictureBox Player2;
         private System.Windows.Forms.PictureBox Mob1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
